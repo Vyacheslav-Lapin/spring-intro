b55a81d41f24312f101cf89d90e91c2c869656ec
Vyacheslav Lapin <vyacheslav@y-tree.com>
2019-09-16 16:05:06 +0300

    JUnit 5 support

--- src/test/java/com/luxoft/training/java/spring/intro/SpringIntroApplicationTests.java
+++ src/test/java/com/luxoft/training/java/spring/intro/SpringIntroApplicationTests.java
@@ -1,6 +1,6 @@
 package com.luxoft.training.java.spring.intro;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.hateoas.MediaTypes.HAL_JSON_UTF8_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -10,43 +10,40 @@ import com.jayway.jsonpath.JsonPath;
 import com.luxoft.training.java.spring.intro.dao.CatRepository;
 import com.luxoft.training.java.spring.intro.model.Cat;
 import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithMockUser(authorities = "ADMIN")
-public class SpringIntroApplicationTests {
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class SpringIntroApplicationTests {
 
-  @Autowired
-  MockMvc mvc;
+  final MockMvc mvc;
 
-  @Autowired
-  CatRepository catRepository;
+  final CatRepository catRepository;
 
-  @Before
-  public void setUp() {
+  @BeforeEach
+  void setUp() {
     Stream.of("Barsik", "Vasya", "Murzik")
         .map(Cat::new)
         .forEach(catRepository::save);
   }
 
   @Test
-  public void contextLoads() {
+  void contextLoads() {
   }
 
   @Test
   @SneakyThrows
-  public void catsPresented() {
+  void catsPresented() {
     mvc.perform(get("/cats"))
 		.andExpect(status().isOk())
 		.andExpect(content()
